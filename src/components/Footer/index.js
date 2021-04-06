bf05781217189336fbe908c609048b9f1ee126af
jelly octane <jamie.de.lange@mac.com>
2021-04-06 17:02:18 -0500

    push

---
+++ src/components/Footer/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Footer() {
+    return (
+        <section>
+            <nav>
+                <div>
+                    <p>built by<a href=""> jamie de lange</a></p>
+                </div>
+            </nav>
+        </section>
+    );
+}
+
+export default Footer;
